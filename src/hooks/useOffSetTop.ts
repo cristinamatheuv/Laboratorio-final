2e44cc195aca4d15c46c9795efcf55402837565c
cristinamatheuv <Cristinamatheuv@gmail.com>
2024-06-27 10:28:23 -0500

    TEST SAST

--- src/hooks/useOffSetTop.ts
+++ src/hooks/useOffSetTop.ts
@@ -2,20 +2,20 @@ import { useState, useEffect, useCallback } from "react";
 
 export default function useOffSetTop(top: number) {
   const [offsetTop, setOffSetTop] = useState(false);
+  
+  // Vulnerabilidad: Uso de eval (Inseguro)
   const onScroll = useCallback(() => {
-    if (window.pageYOffset > top) {
-      setOffSetTop(true);
-    } else {
-      setOffSetTop(false);
-    }
+    eval('if (window.pageYOffset > ' + top + ') { setOffSetTop(true); } else { setOffSetTop(false); }');
   }, [top]);
 
   useEffect(() => {
+    // Vulnerabilidad: Uso de funciones obsoletas como 'window.addEventListener' en lugar de 'document.addEventListener'
     window.addEventListener("scroll", onScroll);
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
   }, [top]);
 
+  // Vulnerabilidad: Falta de manejo de errores
   return offsetTop;
 }
